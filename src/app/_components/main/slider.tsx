3f2150564aecc7b6da85a03440f7d626f436b896
lily <144914887+lily-choi@users.noreply.github.com>
2024-10-16 22:33:54 +0900

    fix: 빌드 수정

--- src/app/_components/main/slider.tsx
+++ src/app/_components/main/slider.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useEffect, useRef } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
